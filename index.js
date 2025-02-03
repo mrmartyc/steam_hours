bdd682d2f018d10e0037c280ad3a0a51c0970ed4
martyc <101498713+mrmartyc@users.noreply.github.com>
2025-02-03 11:25:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const steamUser = require('steam-user');
+const steamTotp = require('steam-totp');
+
+var username = 'mrmartyc_on_ttv';
+var password = 'Vondracek071117/4981';
+var shared_secret = '';
+
+var games = [730, 218, 221100];
+var status = 1
+
+
+user = new steamUser();
+user.logOn({accountName: username, password: password, twoFactorCode: steamTotp.generateAuthCode(shared_secret)});
+user.on('loggedOn', () =>{
+    if (user.steamID != null) console.log(user.steamID + 'Logged in');
+        user.setPersona(status);
+        user.gamesPlayed(games);
+    });
